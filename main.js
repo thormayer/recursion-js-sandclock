8a6bc27b095398255dc0e775dff1a9bf143318cc
thormayer <tsurmayergilad@gmail.com>
2018-05-17 09:15:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,22 @@
 (function(){
   
   var maxStar = 5,
-      minStar = 0,
-      direction = true;
-  
+      minStar = 0;
+
   var functionHolder = function (counter) {
     output(counter);
-    spacing();
-
-    if(getDirection(direction, counter)){
-      if(counter > 1)
-        functionHolder(counter - 1);      
-      else{
-        direction = false;
-        functionHolder(counter + 1);  
-      }  
-    }else{
-      if(counter < maxStar)
-        functionHolder(counter + 1);  
+    if (counter > 1) {
+      functionHolder(counter -1)
     }
+    output(counter);
   }
   
   function output(c){
     for(var i = 0; i < c; i++){
       document.write('*');
     }
-  }
-  
-  function getDirection(dirDesc, currentCounter){
-    return dirDesc ? true : false;
-  }
-  
-  function spacing(){
     document.write('<br/>')
-  }
-  
+  } 
+   
   functionHolder(maxStar);
-  
 })();
